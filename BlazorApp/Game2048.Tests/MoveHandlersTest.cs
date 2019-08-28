9ee86007ca4fd21c4c7a51d0f3a76851cb6f31db
siarhiej <light.drow.777@gmail.com>
2019-08-28 22:49:30 +0200

    Added touch handlers.

--- BlazorApp/Game2048.Tests/MoveHandlersTest.cs
+++ BlazorApp/Game2048.Tests/MoveHandlersTest.cs
@@ -28,7 +28,7 @@ namespace Game2048.Tests
             var state = new GameState {Squares = squares};
             AssignRows(state, numbers);
             
-            var newState = MoveHelper.Handle("ArrowRight", state);
+            var newState = MoveHelper.HandleRight(state);
 
             AssertRows(newState, expectedAfterMove);
         }
@@ -56,7 +56,7 @@ namespace Game2048.Tests
             var state = new GameState { Squares = squares };
             AssignColumns(state, numbers);
 
-            var newState = MoveHelper.Handle("ArrowDown", state);
+            var newState = MoveHelper.HandleDown(state);
 
             AssertColumns(newState, expectedAfterMove);
         }
@@ -84,7 +84,7 @@ namespace Game2048.Tests
             var state = new GameState { Squares = squares };
             AssignRows(state, numbers);
 
-            var newState = MoveHelper.Handle("ArrowLeft", state);
+            var newState = MoveHelper.HandleLeft(state);
 
             AssertRows(newState, expectedAfterMove);
         }
@@ -112,7 +112,7 @@ namespace Game2048.Tests
             var state = new GameState { Squares = squares };
             AssignColumns(state, numbers);
 
-            var newState = MoveHelper.Handle("ArrowUp", state);
+            var newState = MoveHelper.HandleUp(state);
 
             AssertColumns(newState, expectedAfterMove);
         }
